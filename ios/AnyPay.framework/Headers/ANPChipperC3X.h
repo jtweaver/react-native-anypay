05763af2a3c436c3583286a79402f26dee45814e
J.T. Weaver <jtweaver22@gmail.com>
2023-09-17 13:36:47 -0500

    Bump SDK version to 1.90.15

---
+++ ios/AnyPay.framework/Headers/ANPChipperC3X.h
@@ -0,0 +1,13 @@
+//
+//  ANPChipperC3X.h
+//  AnyPay
+//
+//  Created by Ankit Gupta on 15/11/22.
+//  Copyright © 2022 Dan McCann. All rights reserved.
+//
+
+#import <AnyPay/AnyPay.h>
+
+@interface ANPChipperC3X : ANPWalkerC2XBT
+
+@end
